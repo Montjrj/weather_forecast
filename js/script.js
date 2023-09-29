25fd823b4c72dc313e77dd5b8a211883b2dbeac4
Russell Montjoy <russellmontjoy@gmail.com>
2023-09-29 12:40:51 -0600

    call on location and weather API

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 var timeDisplayEl = $('#time-display'); 
+var apiKey = "f073b4eb68aa0b3ed9eedc14f3a8c1b2"
+// var city = document.getElementById("#search_value")
+var city ="Denver"
 
 
 
@@ -9,9 +12,81 @@ function displayTime() {
   }
 
 
+function getLocation() {
+
+    var queryUrl =  "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+
+    fetch(queryUrl)
+     .then(function(response) {  
+            return response.json()
+        })
+    .then(function(data) {
+
+        var lat = data[0].lat
+        var lon = data[0].lon
+
+        console.log(data)
+        console.log(lat)
+        console.log(lon)
+        getCurrentWeather(lat, lon)
+        // getFutureWeather(lat, lon)
+
+        })
+
+
+}
+getLocation()
+
+function getCurrentWeather(lat, lon) {
+
+    var queryUrl =  "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial&lang=en"
+
+    fetch(queryUrl)
+     .then(function(response) {
+            return response.json()
+        })
+    .then(function(data) {
+
+        console.log(data)
+        console.log(data.name)
+        console.log(data.main.temp)
+        console.log(data.main.humidity)
+        console.log(data.wind.speed)
+        console.log(data.dt)
+        console.log(data.weather[0].icon)
+        var dateFormat = dayjs.unix(data.dt).format('D, MMM. YYYY')
+        var dateDisplay = dateFormat
+        var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png"
+        var iconcode = data.weather[0].icon 
+        var name = $("<h2>").text(data.name)
+        var temp = $("<p>").text("Temp: " + Math.round(data.name.temp) + " °F ")
+        var windspeed = $("<p>").text("Wind: " + Math.round(data.wind.speed) + " MPH")
+        var humidity = $("<p>").text("Humidity: " + data.main.humidity + " %")
+        var icon = $("<img>").attr("src", iconurl)
 
 
 
+        })
+
+}
 
 
-  displayTime(); 
+// getFutureWeather() {
+
+//     var queryUrl =  "https://api.openweathermap.org/data/2.5/forecast?q=atlanta&appid=" + apiKey;
+//     fetch(queryUrl)
+//      .then(function(response) {
+//             console.log(response)
+//             return response.json()
+//         })
+//     .then(function(data) {
+
+//         console.log(data)
+//         console.log(data.list[0].main.humidity)
+
+//         })
+
+// }
+
+// getFutureWeather(); 
+ // displayTime(); 
